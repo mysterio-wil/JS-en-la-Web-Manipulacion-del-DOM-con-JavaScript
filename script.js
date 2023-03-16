4227a84f1b9a8c118cb3f645c62a743e6ddb2cd8
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-03-16 12:10:03 -0500

    Aula04 - concluir tarea

--- script.js
+++ script.js
@@ -49,11 +49,20 @@ btn.addEventListener("click", createTask);
 //?Agregado en el aula 04 'creando boton concluir'
 const checkComplete = () => {
     const i = document.createElement("i");
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
+    //? se modificó el agregó en el aula04 'concluir la tarea'
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
 
     return i;
 };
+
+    //? se agregó en el aula04 'concluir la tarea'
+const completeTask = (event) => {
+    /* console.log(event.target); */
+    const element = event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+}
 
 
